cd40b5f57e422facf68970d0347e69efd8fe30a2
Ronald Bennett <bennettrd318@gmail.com>
2017-12-30 15:11:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function deleteFromObjectByKey(object, key) {
 
 function destructivelyDeleteFromObjectByKey(object, key) {
  delete object.key;
- Object.key = 
+ const object = {key};
  return object;
 }
 
